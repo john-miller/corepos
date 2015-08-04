b5de31839a4dc48837a2a2e2ba3a31200646744e
Jonathan Miller <jonathan@winepos.com>
2015-08-04 07:47:00 -0400

    Added component scan to main application class

--- src/main/java/com/corepos/CoreposApplication.java
+++ src/main/java/com/corepos/CoreposApplication.java
@@ -2,8 +2,10 @@ package com.corepos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.corepos")
 public class CoreposApplication {
 
     public static void main(String[] args) {
